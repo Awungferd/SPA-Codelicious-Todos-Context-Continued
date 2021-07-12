545af90f890f8e69c23978b0f5b3c2c60599a656
Joel Peltonen <joel.peltonen@digitalcareerinstitute.org>
2021-07-12 13:00:43 +0200

    Add proper TODO item creation

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -3,27 +3,60 @@ import React from 'react';
 class CreateTodo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            todoText: "-"
+        };
     }
 
     click = () => {
-        // console.log("CreateTODO click function is running!");
+        console.log("CreateTODO click function is running!");
         // Create a TODO item?????
         this.props.createTodo({
-            text: "Buy new keyboard",
+            text: this.state.todoText,
             done: false
         })
+    }
+
+    handleChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            todoText: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form was submitted!");
+
+        // Who remembers how to POST this data?
+        const url = "https://jsonplaceholder.typicode.com/users";
+        const config = {
+            method: "POST",
+            body: JSON.stringify(this.state)
+        };
+        
+        fetch(url, config)
+            .then(response => response.json())
+            .then(result => console.log("Result is", result))
+            .catch(err => console.error(err));
     }
 
     render() {
         console.log("CreateTODO render function is running!");
         return (
-            <div className="todo-create">
-                <input type="text" />
-                <button onClick={this.click}>
+            <form onSubmit={this.handleSubmit}>
+
+                <input
+                    type="text"
+                    value={this.state.todoText}
+                    onChange={this.handleChange}
+                />
+
+                <button type="submit">
                     Add TODO
                 </button>
-            </div>
+
+            </form>
         );
     }
 }
